d0a5a1f1ec262ca49ee248fa63e49390faf88091
loncat <30001379+p4kl0nc4t@users.noreply.github.com>
2021-10-22 00:46:25 +0800

    Code refactoring + a small functional change The small functional change mentioned above refers to the alteration of an empty print at the end of `pokemon.py` file. The empty print is replaced with an escape code for resetting color instead.

--- pokemon.py
+++ pokemon.py
@@ -1,29 +1,27 @@
-from PIL import Image
-from utils import cropImage
-from io import BytesIO
 import random
+from io import BytesIO
+
 import pokebase as pb
+from PIL import Image
 
-block = '██'
+from utils import crop_image
 
+BLOCK = '██'
 TOTAL_POKEMON = 898
 
 pokemon = pb.SpriteResource('pokemon', random.randint(1, TOTAL_POKEMON))
 image = Image.open(BytesIO(pokemon.img_data)).convert('RGBA')
 
-cropped = cropImage(image)
+cropped_image = crop_image(image)
 
-length, width, _ = cropped.shape
+length, width, _ = cropped_image.shape
 for y in range(length):
     for x in range(width):
-        # get RGBA value,
-        r, g, b, a = cropped[y, x, :]
+        r, g, b, a = cropped_image[y, x, :]  # get RGBA value
 
-        # if transparant print [SPACE] ' '
-        if a == 0:
+        if a == 0:  # print whitespace if pixel is transparent (a == 0)
             print('  ', end='')
+        else:  # print colored block
+            print('\x1b[38;2;{};{};{}m'.format(r, g, b) + BLOCK, end='')
 
-        # print colored block
-        else:
-            print('\x1b[38;2;{};{};{}m'.format(r, g, b) + block, end='')
-    print()
+    print('\033[0m')  # reset text color
